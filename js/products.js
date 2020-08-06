07c52e5102b78290fe336eb6e2abbf31b49d81a6
Mateo Álvarez <alvarez.fing@gmail.com>
2020-08-06 00:19:49 -0300

    Imp Productos y Logo
    Visualizacion de los productos en la seccion Productos (beta - falta modificar apariencia y filtrado).
    
    Implementacion del logo (vFinal), solo visible en Home, se debera implementar en toda la pagina

--- js/products.js
+++ js/products.js
@@ -1,6 +1,39 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
+const listaProductos = document.getElementById('lista-productos')
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    // Muestra la lista de productos dentro de products.html
+    fetch(PRODUCTS_URL)
+
+        .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
+        .then(res => res.json())
+        .then(res => {
+
+            const fragment = document.createDocumentFragment()
+
+            for (const infoProducto of res) {
+
+                const producto = document.createElement('A')
+                producto.classList.add('list-group-item', 'list-group-item-action')
+                
+                producto.innerHTML = `
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${infoProducto.imgSrc}" alt="${infoProducto.description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${infoProducto.name}</h4>
+                            <small class="text-muted">${infoProducto.currency} ${infoProducto.cost}</small>
+                        </div>
+                        <p class="mb-1">` + infoProducto.description + `</p>
+                    </div>
+                </div>
+                `
+                fragment.appendChild(producto)
+            }
+
+            listaProductos.appendChild(fragment)
+
+        })
 });
